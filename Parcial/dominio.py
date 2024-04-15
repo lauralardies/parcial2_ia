e7182cd49a5d02f0022b3df2f7964718bb81f233
lauralardies <lrodrlar@myuax.com>
2024-04-15 17:11:30 +0200

    c

--- Parcial/dominio.py
+++ Parcial/dominio.py
@@ -1,8 +1,25 @@
 # Importamos bibliotecas
 import numpy as np
 
-# Creamos un arreglo tridimensional de 10x10x10 con ceros inicialmente
-dominio = np.zeros((10, 10, 10))  
+def creacion_dominio(x, y, z):
+    """
+    Crea un arreglo tridimensional de ceros con las dimensiones especificadas.
+
+    Parámetros:
+        x (int): Número de elementos en la dirección x.
+        y (int): Número de elementos en la dirección y.
+        z (int): Número de elementos en la dirección z.
+
+    Devuelve:
+        dominio (ndarray): Arreglo tridimensional de ceros con las dimensiones especificadas.
+    """
+    # Crea un arreglo tridimensional de ceros con las dimensiones especificadas
+    dominio = np.zeros((x, y, z))
+
+    return dominio
+
+# Ejemplo de uso
+dominio = creacion_dominio(10, 10, 10)
 
 # Mostramos el arreglo tridimensional
 print(dominio)
